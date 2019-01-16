c736ee6895025dc91e4758f84808820f6d913e05
parinita88 <pari.chowdhry@gmail.com>
2019-01-16 22:33:46 +0800

    Flask

--- secondBranch/Hello.py
+++ secondBranch/Hello.py
@@ -1,2 +1,5 @@
-print("Hello World!");
-print("I can write to console");
+from flask import Flask, render_template
+app=Flask(_name_)
+@app.route("/")
+def index():
+    render_template("1.html")
---
+++ secondBranch/Hello0.py
@@ -0,0 +1,18 @@
+i=29
+print(f"i is {i}")
+b=True
+print(f"b is {b}")
+b=False
+print(f"b is {b}")
+n= None
+print(f"n is {n}")
+b = 5
+print(f"b is {b}")
+def square(x):
+    return x*x;
+
+for i in range(10):
+    print(f"{i} squared is {square(i)}")
+
+x = input()
+print(f"Hello {x}");
